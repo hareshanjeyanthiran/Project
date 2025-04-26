410ee07bf961a0d60182a5bb2129e478ba7aa0dd
hareshanjeyanthiran <hareshanjeyanthiran@gmail.com>
2025-04-26 15:04:15 +0400

    Update

---
+++ Project 21/index.js
@@ -0,0 +1,18 @@
+var games = ["Roblox","Fortknite","Krunker","Minecraft","Arena"]
+
+// console.log(games[2])
+
+
+// console.log(games.slice(0,3))
+
+// games.push("Subway")
+// console.log(games)
+
+// games.pop()
+// games.pop()
+ console.log(games)
+
+
+games.forEach((i)=>{
+    console.log(i);
+})
